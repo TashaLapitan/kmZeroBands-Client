28e0e79eb6e343e8ab6b34ae9f3e5542be639c7f
tlapi <tasha.lapitan@gmail.com>
2020-12-15 12:49:07 +0100

    build faq page, add faq route to app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from './pages/Login';
 import MyProfile from './pages/MyProfile';
 import GigBoard from './pages/GigBoard';
 import BandPage from './pages/BandPage';
+import FAQ from './pages/FAQ';
 
 import Navbar from './components/Navbar';
 import AnonRoute from './components/AnonRoute';
@@ -25,6 +26,7 @@ function App () {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/gigboard" component={GigBoard} />
+          <Route exact path="/faq" component={FAQ} />
 
           <AnonRoute exact path="/signup" component={Signup} />
           <AnonRoute exact path="/login" component={Login} />
---
+++ src/pages/FAQ.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function FAQ() {
+    return (
+        <main>
+         <h1>Frequently Asked Questions</h1>
+          <article>
+            <h2>How can I contact a band?</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan lacus dui, non commodo nunc convallis sit amet. In sit amet aliquet eros. Aliquam nibh dolor, tincidunt eu viverra at, viverra ac est. Ut vulputate rhoncus lorem, vitae fringilla ipsum lobortis non. Sed laoreet accumsan enim, ut ullamcorper lorem molestie a.</p>
+          </article>
+          <article>
+            <h2>What if I cannot find a local band that fits my criteria?</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan lacus dui, non commodo nunc convallis sit amet. In sit amet aliquet eros. Aliquam nibh dolor, tincidunt eu viverra at, viverra ac est. Ut vulputate rhoncus lorem, vitae fringilla ipsum lobortis non. Sed laoreet accumsan enim, ut ullamcorper lorem molestie a.</p>
+          </article>
+          <article>
+            <h2>I'm uncomfortable sharing my phone number on this platform. When will you add a messenger?</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan lacus dui, non commodo nunc convallis sit amet. In sit amet aliquet eros. Aliquam nibh dolor, tincidunt eu viverra at, viverra ac est. Ut vulputate rhoncus lorem, vitae fringilla ipsum lobortis non. Sed laoreet accumsan enim, ut ullamcorper lorem molestie a.</p>
+          </article>
+          <article>
+            <h2>I've hired a party band with your website and I loved it! Can I publish them a review so they get more orders?</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan lacus dui, non commodo nunc convallis sit amet. In sit amet aliquet eros. Aliquam nibh dolor, tincidunt eu viverra at, viverra ac est. Ut vulputate rhoncus lorem, vitae fringilla ipsum lobortis non. Sed laoreet accumsan enim, ut ullamcorper lorem molestie a.</p>
+          </article>
+        </main>
+    )
+}
+
+export default FAQ;
+
